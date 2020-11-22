4c3635b31bae25a788077a784a63d089562d24b4
Araceli <a.lobocasas@gmail.com>
2020-11-22 12:48:56 +0100

    Add issues

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import "../styleSheets/styles.css";
 import pokemons from "../data/pokemons";
 import PokeList from "./PokeList";
+import title from "../images/title.png";
+import pikachu2 from "../images/pikachu2.gif";
 
 class App extends React.Component {
   constructor(props) {
@@ -13,9 +15,17 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        <PokeList dataList={this.state.data} />
-      </div>
+      <>
+        <header className="header">
+          <img className="title" src={title} alt="Pokemon title" />
+        </header>
+        <div className="App">
+          <PokeList dataList={this.state.data} />
+        </div>
+        <footer>
+          <img className="pikachu" src={pikachu2} alt="Pikachu" />
+        </footer>
+      </>
     );
   }
 }
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -4,10 +4,10 @@ import PropTypes from "prop-types";
 const Pokemon = (props) => {
   return (
     <div className="poke-li">
-      <h2 className="poke-name">{props.name}</h2>
       <div className="poke-image">
         <img src={props.image} alt={props.name} />
       </div>
+      <h2 className="poke-name">{props.name}</h2>
       <div className="poke-info">
         <div className="poke-types">
           <ul className="types-list">
